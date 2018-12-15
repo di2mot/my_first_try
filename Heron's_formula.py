8256b9852c77781d80031008cfa268688700cfb0
di2mot <caesar92-2008@yandex.ru>
2018-12-15 18:59:38 +0200

    Update Heron's_formula.py

--- Heron's_formula.py
+++ Heron's_formula.py
@@ -1,6 +1,7 @@
+# Heron's formula for finding the area of a triangle
 a = int(input())
 b = int(input())
 c = int(input())
-p = (a + b + c)/2
-S = (p * ((p - a) * (p - b) * (p - c))) ** 0.5
+p = (a + b + c)/2 # half area
+S = (p * ((p - a) * (p - b) * (p - c))) ** 0.5 # the area of the triangle
 print(S)
