f6cfde4e38e61e84e79f50db1a31c3dc08a2e454
di2mot <motsar.dm@gmail.com>
2019-07-21 20:57:38 +0300

    UPD

--- Stepik_32009/1.3 Агрегация данных.sql
+++ Stepik_32009/1.3 Агрегация данных.sql
@@ -9,3 +9,16 @@ from store
 group by category
 order by revenue DESC
 limit 5;
+
+
+
+# Выведите в качестве результата одного запроса общее количество заказов,
+# сумму стоимостей (бюджетов) всех проектов, средний срок исполнения заказа в днях.
+# NB! Для вычисления длительности проекта удобно использовать встроенную функцию datediff().
+
+select 
+    count(project_name) as projects,
+    sum(budget) as sum,
+    avg(datediff(project_finish, project_start)) as time
+from project where project_finish is not null;
+
