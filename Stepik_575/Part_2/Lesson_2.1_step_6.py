165b51c73b4c728e412d25a49c567f0ab0d9d2e7
di2mot <motsar.dm@gmail.com>
2020-02-19 22:00:45 +0200

    Create Lesson_2.1_step_6.py

---
+++ Stepik_575/Part_2/Lesson_2.1_step_6.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+link = 'https://suninjuly.github.io/math.html'
+
+try:
+    #стандартная инициализация
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    people_radio = browser.find_element_by_id("peopleRule")
+
+    people_checked = people_radio.get_attribute("checked")
+    print("value of people radio: ", people_checked)
+    # assert people_checked is not None, "People radio is not selected by default"
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
