84954f37057ca12bb7ef74eacfd1cced37c9927f
di2mot <motsar.dm@gmail.com>
2020-02-23 19:49:42 +0200

    Create Lesson_2.2_tests.py

---
+++ Stepik_575/Part_2/Lesson_2.2_tests.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+link = "https://SunInJuly.github.io/execute_script.html"
+browser.get(link)
+
+button = browser.find_element_by_tag_name("button")
+browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+button.click()
+assert True
+
+# browser = webdriver.Chrome()
+# browser.execute_script("alert('Robots at work');")
