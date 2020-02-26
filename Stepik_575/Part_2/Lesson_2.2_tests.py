76fc74d7ee165daceb2dfddc92b29fe72946a5b5
di2mot <motsar.dm@gmail.com>
2020-02-26 23:09:54 +0200

    Update Lesson_2.2_tests.py

--- Stepik_575/Part_2/Lesson_2.2_tests.py
+++ Stepik_575/Part_2/Lesson_2.2_tests.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 
+text = 'Hello'
 browser = webdriver.Chrome()
-link = "https://SunInJuly.github.io/execute_script.html"
-browser.get(link)
+# link = "https://SunInJuly.github.io/execute_script.html"
+# browser.get(link)
+#
+# button = browser.find_element_by_tag_name("button")
+# browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+# button.click()
+# assert True
+#
+# # browser = webdriver.Chrome()
+browser.execute_script("alert('Robots at work');")
 
-button = browser.find_element_by_tag_name("button")
-browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-button.click()
-assert True
-
-# browser = webdriver.Chrome()
-# browser.execute_script("alert('Robots at work');")
+confirm = browser.switch_to.alert
+print(confirm.text.split(': ')[-1])
+confirm.accept()
